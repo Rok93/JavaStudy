b56233e941c845612198172a2f030acc1035b79e
Administrator <Administrator@DESKTOP-1JL2KJE>
2019-06-20 16:05:13 +0900

    Book, BookMgr, BookStore 제약조건 추가

--- chap6/src/bookTest/Book.java
+++ chap6/src/bookTest/Book.java
@@ -1,5 +1,20 @@
 package bookTest;
-
+/**
+ * * Book 클래스의 제약조건
+ * 1. 책 제목(title)과 가격(price)정보를 저장하는 필드변수가 있어야 한다.
+ * 2. 멤버변수는 모두 private 접근제한자를 가져야 한다. 
+ * 3. 멤버변수를 모두 초기화하는 생성자 메서드가 있어야 한다. : ==>2개 생성자 정의
+ * 4. 멤버변수에 값을 설정하고 리턴하는 setter/getter 메서드가 있어야 한다.
+ * setTitle(String title) : 멤버변수 title 값을 셋팅하는 메소드
+ * String getTitle() : 멤버변수 title 값을 반환하는 메소드
+ * void setPrice(int price) : 멤버변수 price 값을 셋팅하는 메소드
+ * int getPrice() : 멤버변수 price 값을 반환하는 메소드 
+ * 5. setter/getter 메서드는 모두 public 접근제한자를 가져야 한다. 
+ * 
+ * @author Rok93
+ * @version 1.0.0 2019-06-20 Day 15
+ * 
+ */
 public class Book {
 
 	private String title;
